9d1b1d319a7fab22a959d339a63f6e0a973d9bca
Mohammed Sohail <sohailsameja@gmail.com>
2025-07-21 11:10:25 +0300

    feat: add mint data in last-10-tx

--- queries.sql
+++ queries.sql
@@ -1,21 +1,40 @@
 --name: last-10-tx
--- Fetches an account's last 10 transfers
+-- Fetches an account's last 10 transfers and mints
 -- $1: public_key
-SELECT
-    token_transfer.sender_address AS sender,
-    token_transfer.recipient_address AS recipient,
-    token_transfer.transfer_value,
-    token_transfer.contract_address,
-    tx.tx_hash,
-    tx.date_block,
-    tx.success,
-    tokens.token_symbol,
-    tokens.token_decimals
-FROM chain_data.token_transfer
-INNER JOIN chain_data.tx ON token_transfer.tx_id = tx.id
-INNER JOIN chain_data.tokens ON token_transfer.contract_address = tokens.contract_address
-WHERE token_transfer.sender_address = $1 OR token_transfer.recipient_address = $1
-ORDER BY tx.date_block DESC
+(
+    SELECT
+        token_transfer.sender_address AS sender,
+        token_transfer.recipient_address AS recipient,
+        token_transfer.transfer_value,
+        token_transfer.contract_address,
+        tx.tx_hash,
+        tx.date_block,
+        tx.success,
+        tokens.token_symbol,
+        tokens.token_decimals
+    FROM chain_data.token_transfer
+    INNER JOIN chain_data.tx ON token_transfer.tx_id = tx.id
+    INNER JOIN chain_data.tokens ON token_transfer.contract_address = tokens.contract_address
+    WHERE token_transfer.sender_address = $1 OR token_transfer.recipient_address = $1
+)
+UNION ALL
+(
+    SELECT
+        token_mint.minter_address AS sender,
+        token_mint.recipient_address AS recipient,
+        token_mint.mint_value AS transfer_value,
+        token_mint.contract_address,
+        tx.tx_hash,
+        tx.date_block,
+        tx.success,
+        tokens.token_symbol,
+        tokens.token_decimals
+    FROM chain_data.token_mint
+    INNER JOIN chain_data.tx ON token_mint.tx_id = tx.id
+    INNER JOIN chain_data.tokens ON token_mint.contract_address = tokens.contract_address
+    WHERE token_mint.minter_address = $1 OR token_mint.recipient_address = $1
+)
+ORDER BY date_block DESC
 LIMIT 10;
 
 --name: token-holdings
